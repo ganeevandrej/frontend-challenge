9b7c9141c90eca693e28c0eff34c4c2fe4bf6250
ganeev_andrei <ganeevandrej21@gmail.com>
2024-01-31 02:41:22 +0300

    5 version app

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -1,18 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { getImages } from "../../store/reducers/ActionCreators";
 import { CatBlock } from "../helpers/CatBlock";
 
 export const HomePage: React.FC = () => {
-  const cats = useAppSelector((state) => state.CatState.cats);
+  const [scroll, setScroll] = useState<number>(15);
+  const {cats, isLoading }  = useAppSelector((state) => state.CatState);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getImages());
-  }, []);
+    document.addEventListener("scroll", hendlerScroll);
+    dispatch(getImages(scroll));
+    return () => {
+      document.removeEventListener("scroll", hendlerScroll);
+    };
+  }, [scroll, dispatch]);
+
+  const hendlerScroll = () => {
+    const element = document.documentElement;
+    if(element.scrollHeight - (element.scrollTop + window.innerHeight) < 70) {
+      console.log("vhvjh");
+      setScroll((state) => state + 5);
+    }
+  };
+
+  const loading = <div className="loading">... загружаем еще котиков ...</div>;
+
   return (
-    <div className="block_kats">
-      <CatBlock cats={cats} />
-    </div>
+    <>
+      <div className="block_kats">
+        <CatBlock cats={cats} />
+      </div>
+      { 
+        isLoading && loading
+      }
+    </>
   );
 };
--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -5,11 +5,14 @@ import axios from 'axios';
 import { transformCat } from '../../components/helpers/trnsformCat';
 
 
-export const getImages = () => async (dispatch: AppDispatch) => {
+export const getImages = (limit: number = 10) => async (dispatch: AppDispatch) => {
     try {
         dispatch(CatSlice.actions.imagesFetching());
-        const response = await axios.get<ICat[]>("https://api.thecatapi.com/v1/images/search?limit=10");
-
+        const response = await axios.get<ICat[]>(`https://api.thecatapi.com/v1/images/search?limit=${limit}`, {
+            headers: {
+                'x-api-key': 'live_7K0GEoWfeVTF4ITXkvrLo4RlzMKqNKqbSQHAxnHQAW72T4f4OUvCiZuNJtIYagXf',
+            }
+        });
         dispatch(CatSlice.actions.imagesFetchingSuccess(response.data.map(transformCat)));
     } catch (e) {
         dispatch(CatSlice.actions.imagesFetchingError('произошла ошибка'));
